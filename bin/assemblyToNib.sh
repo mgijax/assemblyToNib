8fba4b2114bf5e2245768a414c83e6e75f1b6af9
Sharon Giannatto <Sharon.Giannatto@jax.org>
2004-12-23 16:00:51 +0000

    added logic to store the FASTA files

--- bin/assemblyToNib.sh
+++ bin/assemblyToNib.sh
@@ -54,6 +54,7 @@ cd `dirname $0`/..
 LOG=`pwd`/assemblyToNib.log
 rm -f ${LOG}
 
+date | tee ${LOG}
 #
 #  Verify the argument(s) to the shell script.
 #
@@ -70,7 +71,7 @@ fi
 CONFIG=`pwd`/Configuration
 
 #
-#  Make sure the configuration file is readable.
+#  Make sure the configuration file is readable then source it
 #
 if [ ! -r ${CONFIG} ]
 then
@@ -78,12 +79,12 @@ then
     exit 1
 fi
 
-#
-# Source the configuration file
-#
 . ${CONFIG}
 
+#
 # get the chromosome list for the requested organism
+#
+
 if [ ${ORGANISM} = "mouse" ]
 then
     chrList=$mouseChrList
@@ -97,48 +98,67 @@ else
     exit 1
 fi
 
+#
+# clean out the output directories
+#
 
-# clean out the output directory
-echo "removing all files from ${OUTPUTDIR}" | tee -a ${LOG}
-rm -f ${OUTPUTDIR}/*
+echo "removing all files from ${NIB_OUTPUTDIR}" | tee -a ${LOG}
+rm -f ${NIB_OUTPUTDIR}/*
 
+echo "removing all files from ${FA_OUTPUTDIR}" | tee -a ${LOG}
+rm -f ${FA_OUTPUTDIR}/*
+
+#
 # go to the input directory and get the list of chromosome files 
+#
+
 cd ${INPUTDIR}
 echo "pwd: `pwd`"
 zipped_files=`ls ${FILENAME_PATTERN}`
 
-# unzip the list of files in the input directory
+#
+# unzip the chromosome files to the FASTA output directory
+# 
 for f in ${zipped_files}
 do
 	# get filename without the compression extension
 	prefix=`echo $f | sed "s/${ZIP_EXT}//"`
-	echo "uncompressing ${INPUTDIR}/$f to ${OUTPUTDIR}/${prefix}" | tee -a ${LOG}
+	echo "uncompressing ${INPUTDIR}/$f to ${FA_OUTPUTDIR}/${prefix}" | tee -a ${LOG}
 
 	# decompress to output directory
-	${ZIP_UTILITY} $f > ${OUTPUTDIR}/${prefix}
+	${ZIP_UTILITY} $f > ${FA_OUTPUTDIR}/${prefix}
 done 
 
-echo "" | tee -a ${LOG}
+#
+# go to the fasta dir and rename the files 
+#
+# e.g renaming *.1.* to chr1.fa
+#
+# Ensembl and UCSC have different chars pre and post chromosome number in 
+# their filenames thus the use of PRE_CHAR and POST_CHAR
+cd ${FA_OUTPUTDIR}
 
-# go to the output dir
-cd ${OUTPUTDIR}
-
-# get the list of unzipped chromosome files
-unzipped_files=`ls *${UNZIPPED_EXT}`
-
-# faToNib each file adding nib extension and removing unzipped file
-for f in ${unzipped_files}
-do   
-	echo  "Performing faToNib on $f and writing to ${outputdir}/${f}.to${NIB_EXT}" | tee -a ${LOG}
-        ${FATONIB} $f $f.to${NIB_EXT}
-	rm $f
-done 
-
-echo "" | tee -a ${LOG}
-
-# rename each file
 for chr in ${chrList}
 do
-        echo "renaming *${PRE_CHR}${chr}${POST_CHR}* to chr${chr}${NIB_EXT}" | tee -a ${LOG}
-        mv *${PRE_CHR}$chr${POST_CHR}* chr${chr}${NIB_EXT}
+        echo "renaming *${PRE_CHR}${chr}${POST_CHR}* to chr${chr}${FA_EXT}" | tee -a ${LOG}
+        mv *${PRE_CHR}$chr${POST_CHR}* chr${chr}${FA_EXT}
 done
+
+echo "" | tee -a ${LOG}
+
+#
+# get the list of FASTA chromosome files and faToNib them to the 
+# NIB output dir
+#
+# e.g. Performing faToNib on chr1.fa and writing to /data/research/dna/build_nib/chr1.nib
+
+unzipped_files=`ls *${UNZIPPED_EXT}`
+
+# faToNib each file adding nib extension 
+for f in ${unzipped_files}
+do   	prefix=`echo $f | sed "s/${FA_EXT}//"`
+	echo  "Performing faToNib on $f and writing to ${NIB_OUTPUTDIR}/`basename $prefix`${NIB_EXT}" | tee -a ${LOG}
+        ${FATONIB} $f ${NIB_OUTPUTDIR}/`basename $prefix`${NIB_EXT}
+done 
+
+date | tee -a ${LOG}
